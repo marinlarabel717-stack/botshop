9beef24f7967dc4054071ea74ac00db8b9778fa9
marinlarabel717-stack <marinlarabel717-stack@users.noreply.github.com>
2026-05-07 00:02:51 -0700

    style: simplify account check progress text

--- haopubot.py
+++ haopubot.py
@@ -76,6 +76,7 @@ MOOD_EMOJI_FIRE = '[emoji:5220166546491459639:🔥]'
 ACCOUNT_CHECK_EMOJI_PROGRESS = '[emoji:5296562641613897196:🕜]'
 ACCOUNT_CHECK_EMOJI_ALIVE = '[emoji:5260463209562776385:✅]'
 ACCOUNT_CHECK_EMOJI_INVALID = '[emoji:5273914604752216432:❌]'
+ACCOUNT_CHECK_EMOJI_FROZEN = '[emoji:5449449325434266744:❄️]'
 ACCOUNT_CHECK_EMOJI_TIMEOUT = '[emoji:5382194935057372936:⏱️]'
 ACCOUNT_CHECK_EMOJI_TOTAL = '[emoji:5352625743081775722:🎚️]'
 
@@ -1866,11 +1867,7 @@ def build_purchase_success_header(deducted_amount, remaining_amount):
 def build_account_check_progress_text(total_count, checked_count, alive_count=0, invalid_count=0, frozen_count=0, timeout_count=0):
     return (
         f'<b>{ACCOUNT_CHECK_EMOJI_PROGRESS} 正在检查账号状态，请稍等！</b>\n\n'
-        f'<b>{ACCOUNT_CHECK_EMOJI_TOTAL} 已检测：</b> {checked_count} / {total_count}\n'
-        f'<b>{ACCOUNT_CHECK_EMOJI_ALIVE} 存活账号：</b> {alive_count}\n'
-        f'<b>{ACCOUNT_CHECK_EMOJI_INVALID} 无效账号：</b> {invalid_count}\n'
-        f'<b>{ACCOUNT_CHECK_EMOJI_INVALID} 冻结账号：</b> {frozen_count}\n'
-        f'<b>{ACCOUNT_CHECK_EMOJI_TIMEOUT} 超时账号：</b> {timeout_count}'
+        f'<b>{ACCOUNT_CHECK_EMOJI_TOTAL} 已检测：</b> {checked_count} / {total_count}'
     )
 
 
@@ -1888,7 +1885,7 @@ def build_account_check_result_text(total_count, alive_count, invalid_count, fro
         f'<b>{ACCOUNT_CHECK_EMOJI_TOTAL} 账号数量：</b> {total_count}',
         f'<b>{ACCOUNT_CHECK_EMOJI_ALIVE} 存活账号：</b> {alive_count}',
         f'<b>{ACCOUNT_CHECK_EMOJI_INVALID} 无效账号：</b> {invalid_count}',
-        f'<b>{ACCOUNT_CHECK_EMOJI_INVALID} 冻结账号：</b> {frozen_count}',
+        f'<b>{ACCOUNT_CHECK_EMOJI_FROZEN} 冻结账号：</b> {frozen_count}',
     ])
     if timeout_count:
         lines.append(f'<b>{ACCOUNT_CHECK_EMOJI_TIMEOUT} 超时账号：</b> {timeout_count}')
